1bf03d8f777d678462f7e016296ee63e208cb3d6
Danish Farooqui <danishfarooqui2018@gmail.com>
2024-07-21 20:32:38 +0530

    js intro

---
+++ conditions.js
@@ -0,0 +1,8 @@
+n=88;
+
+if(n%2 === 0){
+    console.log('even')
+}
+else{
+    console.log('odd')
+}
---
+++ index.js
@@ -0,0 +1,10 @@
+ console.log('Hello javascript');
+ console.log('Nice')
+
+ num=1264;
+ myname='Danish';
+ console.log(myname);
+
+ console.log(typeof num);
+ console.log(typeof myname);
+ 
